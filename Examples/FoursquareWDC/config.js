882b6fbd5754d4adedcc8f00459769187569013b
aterakrofi <aterakrofi@gmail.com>
2020-07-08 19:57:52 -0700

    port 3333

---
+++ Examples/FoursquareWDC/config.js
@@ -0,0 +1,10 @@
+// The necessary configuration for your server
+// Contains credentials for your Foursquare application
+// And the new redirect path for the OAuth flow
+module.exports = {
+ 'HOSTPATH': 'https://localhost',
+ 'PORT' : 3333,
+ 'CLIENT_ID': 'TJA2EKSTKY13M3JYJDQCMMJYSSQYCPQEORNVAUIWTV3A0TCS',
+ 'CLIENT_SECRET': 'EXXC0UUDFONRFX4KPGORE31PVHALKKAPBSVUSLUODWMXWZ04',
+ 'REDIRECT_PATH': '/redirect'
+};
